6679e258bd61a59271a7d0fb0a5c447e35ec25e6
clarysse allyssa sarmiento <clarysseallyssa.sarmiento@mail.polimi.it>
2024-09-30 22:44:16 +0200

    added print of the stages: magnitude, filter, remove_mean,autocorrelation,derivative

--- src/autocorrelation/autocorrelation_stepcount.h
+++ src/autocorrelation/autocorrelation_stepcount.h
@@ -14,6 +14,14 @@
 #define autocorrelation_stepcount_h
 #include "stdint.h"
  
+#define DUMP_FILE
+#define DUMP_MAGNITUDE_FILE_NAME "magnitude.csv"
+#define DUMP_FILTERED_FILE_NAME "filtered.csv"
+#define DUMP_REMOVED_MEAN_FILE_NAME "removed_mean.csv"
+#define DUMP_AUTOCORRELATION_FILE_NAME "autocorrelation.csv"
+#define DUMP_DERIVATIVE_FILE_NAME "derivative.csv"
+
+
 #define SAMPLING_RATE           12.5                    //20 hz sampling rate
 #define NUM_TUPLES 80
 // Definisci NUM_TUPLES come il numero di tuple contate
