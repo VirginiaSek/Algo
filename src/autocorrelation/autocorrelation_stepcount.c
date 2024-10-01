9bb5bfa18bcdf9d2e904aaaf01031db7d51a229c
Dario Salvi <dariosalvi78@gmail.com>
2024-10-01 14:15:43 +0200

    simplified autocorr algo

--- src/autocorrelation/autocorrelation_stepcount.c
+++ src/autocorrelation/autocorrelation_stepcount.c
@@ -11,11 +11,13 @@ static FILE *removedMeanFile;
 static FILE *autocorrelationFile;
 static FILE *derivativeFile;
 #endif
-#define NUM_AUTOCORR_LAGS 50                 // number of lags to calculate for autocorrelation. 50 lags @20Hz corresponds to a step rate of 0.4Hz...its probably not physically possible to walk much slower than this
-#define DERIV_FILT_LEN 7                     // length of derivative filter
-#define LPF_FILT_LEN 9                       // length of FIR low pass filter
-#define AUTOCORR_DELTA_AMPLITUDE_THRESH 1000 // this is the min delta between peak and trough of autocorrelation peak
-#define AUTOCORR_MIN_HALF_LEN 3              // this is the min number of points the autocorrelation peak should be on either side of the peak
+#define NUM_AUTOCORR_LAGS 15      // number of lags to calculate for autocorrelation. At a minimum step rate of §1 step /s -> 1000 / sampling_period(ms) = sampling frequency -> add a few more -> 15 at 12.5 Hz
+#define FIRST_AUTOCORR_PEAK_LAG 4 // corresponds to the first feasible autocorrelation lag -> at a max step rate of 3 steps /s (running) -> 333ms / sampling_period(ms) = 4 at 12.5 Hz
+
+#define DERIV_FILT_LEN 7                    // length of derivative filter
+#define LPF_FILT_LEN 9                      // length of FIR low pass filter
+#define AUTOCORR_DELTA_AMPLITUDE_THRESH 5e8 // this is the min delta between peak and trough of autocorrelation peak
+#define AUTOCORR_MIN_HALF_LEN 2             // this is the min number of points the autocorrelation peak should be on either side of the peak
 
 static int8_t deriv_coeffs[DERIV_FILT_LEN] = {-6, 31, 0, -31, 6};
 static int16_t lpf_coeffs[LPF_FILT_LEN] = {-2696, -3734, 11354, 17457, 11354, -3734, -2696};
@@ -212,6 +214,72 @@ static void derivative(int64_t *autocorr_buff, int64_t *deriv)
 #endif
 }
 
+// use the original autocorrelation signal to hone in on the
+// exact peak index. this corresponds to the point where the points to the
+// left and right are less than the current point
+static uint8_t get_precise_peakind(int64_t *autocorr_buff, uint8_t peak_ind)
+{
+    uint8_t loop_limit = 0;
+    if ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind - 1]) && (autocorr_buff[peak_ind] > autocorr_buff[peak_ind + 1]))
+    {
+        // peak_ind is perfectly set at the peak. nothing to do
+    }
+    else if ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind + 1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind - 1]))
+    {
+        // peak is to the left. keep moving in that direction
+        loop_limit = 0;
+        while ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind + 1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind - 1]) && (loop_limit < 10))
+        {
+            peak_ind = peak_ind - 1;
+            loop_limit++;
+        }
+    }
+    else
+    {
+        // peak is to the right. keep moving in that direction
+        loop_limit = 0;
+        while ((autocorr_buff[peak_ind] > autocorr_buff[peak_ind - 1]) && (autocorr_buff[peak_ind] < autocorr_buff[peak_ind + 1]) && (loop_limit < 10))
+        {
+            peak_ind = peak_ind + 1;
+            loop_limit++;
+        }
+    }
+    return peak_ind;
+}
+
+// take a look at the original autocorrelation signal at index i and see if
+// it's a real peak or if it's just a fake "noisy" peak corresponding to
+// non-walking. Basically just count the number of points of the
+// autocorrelation peak to the right and left of the peak. this function gets
+// the number of points to the right and left of the peak, as well as the delta amplitude
+static void get_autocorr_peak_stats(int64_t *autocorr_buff, uint8_t *neg_slope_count, int64_t *delta_amplitude_right, uint8_t *pos_slope_count, int64_t *delta_amplitude_left, uint8_t peak_ind)
+{
+
+    // first look to the right of the peak. walk forward until the slope begins decreasing
+    uint8_t neg_slope_ind = peak_ind;
+    uint16_t loop_limit = NUM_AUTOCORR_LAGS - 1;
+    while ((autocorr_buff[neg_slope_ind + 1] - autocorr_buff[neg_slope_ind] < 0) && (neg_slope_ind < loop_limit))
+    {
+        *neg_slope_count = *neg_slope_count + 1;
+        neg_slope_ind = neg_slope_ind + 1;
+    }
+
+    // get the delta amplitude between peak and right trough
+    *delta_amplitude_right = autocorr_buff[peak_ind] - autocorr_buff[neg_slope_ind];
+
+    // next look to the left of the peak. walk backward until the slope begins increasing
+    uint8_t pos_slope_ind = peak_ind;
+    loop_limit = 0;
+    while ((autocorr_buff[pos_slope_ind] - autocorr_buff[pos_slope_ind - 1] > 0) && (pos_slope_ind > loop_limit))
+    {
+        *pos_slope_count = *pos_slope_count + 1;
+        pos_slope_ind = pos_slope_ind - 1;
+    }
+
+    // get the delta amplitude between the peak and the left trough
+    *delta_amplitude_left = autocorr_buff[peak_ind] - autocorr_buff[pos_slope_ind];
+}
+
 // Algorithm to count steps
 steps_t autcorr_count_steps(accel_big_t *mag_sqrt)
 {
@@ -233,12 +301,11 @@ steps_t autcorr_count_steps(accel_big_t *mag_sqrt)
     // corresponds to the first positive peak in the autocorrelation. look at two samples
     // instead of just one to maybe reduce the chances of getting tricked by noise
     uint8_t peak_ind = 0;
-    // start index is set to 8 lags, which corresponds to a walking rate of 2.5Hz @20Hz sampling rate. its probably
-    // running if its faster than this
+    // start index is set to a rate that is feasible when running
     uint16_t i;
-    for (i = 8; i < NUM_AUTOCORR_LAGS; i++)
+    for (i = FIRST_AUTOCORR_PEAK_LAG; i < NUM_AUTOCORR_LAGS; i++)
     {
-        if ((deriv[i] > 0) && (deriv[i - 1] > 0) && (deriv[i - 2] < 0) && (deriv[i - 3] < 0))
+        if ((deriv[i] < 0) && (deriv[i - 1] < 0) && (deriv[i - 2] > 0) && (deriv[i - 3] > 0))
         {
             peak_ind = i - 1;
             break;
@@ -246,32 +313,36 @@ steps_t autcorr_count_steps(accel_big_t *mag_sqrt)
     }
 
     // hone in on the exact peak index
-    peak_ind = get_precise_peakind(autocorr_buff, peak_ind);
+    // peak_ind = get_precise_peakind(autocorr_buff, peak_ind);
     // printf("peak ind: %i\n", peak_ind);
 
-    // get autocorrelation peak stats
-    uint8_t neg_slope_count = 0;
-    int64_t delta_amplitude_right = 0;
-    uint8_t pos_slope_count = 0;
-    int64_t delta_amplitude_left = 0;
-    get_autocorr_peak_stats(autocorr_buff, &neg_slope_count, &delta_amplitude_right, &pos_slope_count, &delta_amplitude_left, peak_ind);
+    uint8_t num_steps = 0;
 
     // now check the conditions to see if it was a real peak or not, and if so, calculate the number of steps
-    uint8_t num_steps = 0;
-    if ((pos_slope_count > AUTOCORR_MIN_HALF_LEN) && (neg_slope_count > AUTOCORR_MIN_HALF_LEN) && (delta_amplitude_right > AUTOCORR_DELTA_AMPLITUDE_THRESH) && (delta_amplitude_left > AUTOCORR_DELTA_AMPLITUDE_THRESH))
-    {
-        // the period is peak_ind/sampling_rate seconds. that corresponds to a frequency of 1/period
-        // with the frequency known, and the number of seconds is 4 seconds, you can then find out the number of steps
-        num_steps = (SAMPLING_RATE * WINDOW_LENGTH) / peak_ind;
-    }
-    else
-    {
-        // not a valid autocorrelation peak
-        num_steps = 0;
-    }
+    // get autocorrelation peak stats
+    // uint8_t neg_slope_count = 0;
+    // int64_t delta_amplitude_right = 0;
+    // uint8_t pos_slope_count = 0;
+    // int64_t delta_amplitude_left = 0;
+    // get_autocorr_peak_stats(autocorr_buff, &neg_slope_count, &delta_amplitude_right, &pos_slope_count, &delta_amplitude_left, peak_ind);
+    // if ((pos_slope_count > AUTOCORR_MIN_HALF_LEN) && (neg_slope_count > AUTOCORR_MIN_HALF_LEN) && (delta_amplitude_right > AUTOCORR_DELTA_AMPLITUDE_THRESH) && (delta_amplitude_left > AUTOCORR_DELTA_AMPLITUDE_THRESH))
+    // {
+    //     // the period is peak_ind/sampling_rate seconds. that corresponds to a frequency of 1/period
+    //     // with the frequency known, and the number of seconds is 4 seconds, you can then find out the number of steps
+    //     num_steps = (SAMPLING_RATE * WINDOW_LENGTH) / peak_ind;
+    // }
+    // else
+    // {
+    //     // not a valid autocorrelation peak
+    //     num_steps = 0;
+    // }
 
-    return num_steps;
+    num_steps = (SAMPLING_RATE * WINDOW_LENGTH) / peak_ind;
+
 #ifdef DUMP_FILE
     close_dump_files();
 #endif
+
+    // printf("num steps: %i\n", num_steps);
+    return num_steps;
 }
